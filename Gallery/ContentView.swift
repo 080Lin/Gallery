8c1d577ea6b1c9bb5ee5d7848bc811f2f18377ee
Maxim Vainikka <maxim.vainikka@yandex.ru>
2022-01-20 21:06:16 +0300

    Initial Commit

---
+++ Gallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gallery
+//
+//  Created by Максим Нуждин on 20.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by Максим Нуждин on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
